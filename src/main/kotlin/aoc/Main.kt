6fb970f3c87371764714bb690d31a6b535791c65
Alistair O'Neill <aon@juxt.pro>
2021-12-12 14:52:41 +0000

    Day12

--- src/main/kotlin/aoc/Main.kt
+++ src/main/kotlin/aoc/Main.kt
@@ -1,12 +1,13 @@
 package aoc
 
 import aoc.day11.day11
+import aoc.day12.day12
 import java.time.Instant
 
 
 fun main() {
     val before = Instant.now().toEpochMilli()
-    day11()
+    day12()
     val after = Instant.now().toEpochMilli()
     println("${after - before}ms")
 }
---
+++ src/main/kotlin/aoc/day12/Day12.kt
@@ -0,0 +1,93 @@
+package aoc.day12
+
+import aoc.day12.CaveId.Companion.END
+import aoc.day12.CaveId.Companion.START
+import java.io.File
+
+fun day12() {
+    File("data/2021/real/12_01.txt")
+        .readText()
+        .let { RouteSolver(it, 1) }
+        .getAllRoutes()
+        .size
+        .let(::println)
+}
+
+
+
+private data class CaveId(val value: String) {
+    companion object {
+        val START = CaveId("start")
+        val END = CaveId("end")
+    }
+}
+
+private class RouteSolver(input: String, private val maxSmallRevisits: Int) {
+
+    private abstract inner class CaveNode(val id: CaveId) {
+        protected val links = mutableSetOf<CaveNode>()
+        abstract fun headTowards(history: List<CaveNode>): List<List<CaveNode>>
+
+        fun linkTo(other: CaveNode) {
+            links.add(other)
+        }
+    }
+
+    private fun factoryNode(id: CaveId) =
+        when {
+            id == START -> StartNode()
+            id == END -> EndNode()
+            id.value.all(Char::isUpperCase) -> BigNode(id)
+            id.value.all(Char::isLowerCase) -> SmallNode(id)
+            else -> error("Invalid CaveId[${id.value}]")
+        }
+
+    private inner class StartNode: CaveNode(START) {
+        fun start(): List<List<CaveNode>> = links.flatMap { it.headTowards(listOf(this)) }
+        override fun headTowards(history: List<CaveNode>): List<List<CaveNode>> = emptyList()
+    }
+
+    private inner class BigNode(id: CaveId): CaveNode(id) {
+        override fun headTowards(history: List<CaveNode>): List<List<CaveNode>> = links.flatMap { it.headTowards(history + this) }
+    }
+
+    private inner class SmallNode(id: CaveId): CaveNode(id) {
+        override fun headTowards(history: List<CaveNode>) =
+            if (history.smallRevisits >= maxSmallRevisits && history.contains(this)) emptyList()
+            else links.flatMap { it.headTowards(history + this) }
+    }
+
+    private inner class EndNode: CaveNode(END) {
+        override fun headTowards(history: List<CaveNode>) = listOf(history)
+    }
+
+    private val start: StartNode
+
+    private val List<CaveNode>.smallRevisits get() =
+        filterIsInstance<SmallNode>().run {
+            size - distinct().size
+        }
+
+    init {
+        val links = input.split("\n").map {
+            val split = it.split("-")
+            CaveId(split[0]) to CaveId(split[1])
+        }
+
+        val nodes = links.flatMap(Pair<CaveId, CaveId>::toList).toSet().associateWith(::factoryNode)
+
+        fun get(id: CaveId): CaveNode = nodes[id]!!
+
+        links.forEach { (fromId, toId) ->
+            val from = get(fromId)
+            val to = get(toId)
+            from.linkTo(to)
+            to.linkTo(from)
+        }
+
+        start = get(START) as StartNode
+    }
+
+    fun getAllRoutes(): List<List<CaveId>> = start.start().map { it.map(CaveNode::id) }
+}
+

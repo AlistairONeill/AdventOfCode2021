d77d53cd0d3c5fb788e8eed56c0d395a7e81b963
Alistair O'Neill <aon@juxt.pro>
2021-12-14 08:16:30 +0000

    It... works?

--- src/main/kotlin/aoc/Main.kt
+++ src/main/kotlin/aoc/Main.kt
@@ -1,14 +1,12 @@
 package aoc
 
-import aoc.day11.day11
-import aoc.day12.day12
-import aoc.day13.day13
+import aoc.day14.day14
 import java.time.Instant
 
 
 fun main() {
     val before = Instant.now().toEpochMilli()
-    day13()
+    day14()
     val after = Instant.now().toEpochMilli()
     println("${after - before}ms")
 }
---
+++ src/main/kotlin/aoc/day14/Day14.kt
@@ -0,0 +1,78 @@
+package aoc.day14
+
+import java.io.File
+
+fun day14() {
+    File("data/2021/real/14_01.txt")
+        .readText()
+        .let(::PolymerisationSimulator)
+        .run {
+            getFinalCounts(40)
+                .run {
+                    maxOf { it } - minOf { it }
+                }.let(::println)
+
+        }
+}
+
+class PolymerisationSimulator(
+    input: String
+) {
+    private val knowledge = hashMapOf<Triple<Char, Char, Int>, ElementCounts>()
+    private val start: List<Char>
+    private val recipes : Map<Pair<Char, Char>, Char>
+
+    init {
+        val (start, rawRecipes) = input.split("\n\n")
+        this.start = start.toList()
+
+        recipes = rawRecipes.split("\n")
+            .associate { line -> (line[0] to line[1]) to line[6] }
+    }
+
+    private fun getElementCounts(a: Char, b: Char, steps: Int): ElementCounts =
+        knowledge[Triple(a, b, steps)]
+            ?: if (steps == 0) {
+                emptyMap()
+            } else {
+                val new = recipes[a to b]
+                if (new == null) {
+                    mapOf(a to 1L, b to 1L)
+                } else {
+                    combine(
+                        getElementCounts(a, new, steps-1),
+                        getElementCounts(new, b, steps-1),
+                        new
+                    )
+                }
+            }.also {
+                knowledge[Triple(a, b, steps)] = it
+            }
+
+    private fun combine(count1: ElementCounts, count2: ElementCounts, dupe: Char): ElementCounts =
+        (count1.keys.union(count2.keys) + dupe)
+            .associateWith {
+                (count1[it] ?: 0L) + (count2[it] ?: 0L) - if(dupe==it) 1L else 0L
+            }
+
+    fun getFinalCounts(steps: Int): Collection<Long> =
+        start.zipWithNext()
+            .map { (a, b) -> a to getElementCounts(a, b, steps) }
+            .fold(
+                mapOf(
+                    start.first() to -1L,
+                    start.last() to -1L
+                )
+            ) { acc, (dupe, counts) ->
+                combine(
+                    acc,
+                    counts,
+                    dupe
+                )
+            }.also {
+                println(it)
+            }
+            .values
+}
+
+typealias ElementCounts = Map<Char, Long>

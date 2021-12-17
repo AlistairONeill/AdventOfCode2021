46f1e5cbeed1ae580c1eb8a8ebb7f95c2e953938
Alistair O'Neill <aon@juxt.pro>
2021-12-17 23:04:32 +0000

    Windows go brr

--- src/main/kotlin/aoc/day1/Day1.kt
+++ src/main/kotlin/aoc/day1/Day1.kt
@@ -3,21 +3,18 @@ package aoc.day1
 import java.io.File
 
 fun day1() {
-    File("data/2021/real/01_01.txt").readLines()
+    File("data/2021/real/01_01.txt")
+        .readLines()
         .map(String::toInt)
         .zipWithNext()
         .count { (left, right) -> right > left }
         .let(::println)
 
-    File("data/2021/real/01_01.txt").readLines()
+    File("data/2021/real/01_01.txt")
+        .readLines()
         .map(String::toInt)
-        .toRollingSum(3)
+        .windowed(3, transform = List<Int>::sum)
         .zipWithNext()
         .count { (left, right) -> right > left }
         .let(::println)
 }
-
-private fun List<Int>.toRollingSum(averageSize: Int) =
-    (0 until size + 1 - averageSize).map {
-        (it until it + averageSize).sumOf(this::get)
-    }
